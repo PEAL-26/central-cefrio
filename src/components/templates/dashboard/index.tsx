5406e793a862403a0a4168a975a43643aefece9d
peal-26 <edilasio@live.com>
2024-08-06 01:36:03 +0100

    deploy

--- src/components/templates/dashboard/index.tsx
+++ src/components/templates/dashboard/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "../../../components/ui/card";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
